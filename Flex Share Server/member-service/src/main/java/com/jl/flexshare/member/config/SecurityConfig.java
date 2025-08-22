72f7c6e4203813843f441142b3bba4725ecd3ef7
JL37776 <qik236899@gmail.com>
2025-08-23 07:09:13 +1200

    feat: add SpringBoot Security support

--- Flex Share Server/member-service/src/main/java/com/jl/flexshare/member/config/SecurityConfig.java
+++ Flex Share Server/member-service/src/main/java/com/jl/flexshare/member/config/SecurityConfig.java
@@ -1,19 +1,47 @@
 package com.jl.flexshare.member.config;
 
+import com.jl.flexshare.member.component.LoginAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Every unauthorized api access will redirect to entryPoint.
+    @Autowired
+    LoginAuthenticationEntryPoint authenticationEntryPoint;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/users/")
-                .permitAll();
+                .antMatchers( "/users/register","/users/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();}
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- Flex Share Server/member-service/src/main/java/com/jl/flexshare/member/controller/UserController.java
+++ Flex Share Server/member-service/src/main/java/com/jl/flexshare/member/controller/UserController.java
@@ -1,6 +1,4 @@
 package com.jl.flexshare.member.controller;
-
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jl.flexshare.member.result.ErrorType;
 import com.jl.flexshare.member.result.Result;
@@ -11,11 +9,17 @@ import com.jl.flexshare.member.validation.LoginGroup;
 import com.jl.flexshare.member.validation.SignUpGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+
 
 @RestController
 @RequestMapping("/users")
@@ -24,18 +28,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @PostMapping("/register")
     public ResponseEntity<Result> register(@Validated({SignUpGroup.class}) @RequestBody User user) {
-
+        
         QueryWrapper<User> query = new QueryWrapper<>();
+        query.eq("email",user.getEmail());
+        User qy = userService.getOne(query);
+        if(qy != null) {
+            return
+                    ResponseEntity.ok(Result.error(ResultError.error(ErrorType.User_exist)));
+        }
 
-        //1.check if user email exist
-//        User qy = userService.getOne(query);
-//        if(qy != null) {
-//            return
-//                    ResponseEntity.ok(Result.error(ResultError.error(ErrorType.User_exist)));
-//        }
-
+        //encoding password
+        user.setPassword(encoder.encode(user.getPassword()));
         boolean res = userService.save(user);
         Result result =res?
                 Result.success()
@@ -44,12 +52,23 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity login(@Validated({LoginGroup.class}) @RequestBody User user) {
+    public ResponseEntity login(@Validated({LoginGroup.class}) @RequestBody User user, HttpServletRequest request ) {
         boolean res = userService.loginMember(user);
-        System.out.println(res);
+
         Result result =res?
                 Result.success()
                 :Result.error(ResultError.error(ErrorType.Login_failed));
+
+        //generate unique authToken
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+
+        //set token into current context;
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        request.getSession(true).setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY
+                , SecurityContextHolder.getContext() );
+
         return ResponseEntity.ok(result);
     }
 }
