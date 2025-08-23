b016e8a00f14875a00cd6923971092f45efbd798
dinghw <dhw992233@gmail.com>
2025-08-23 17:51:42 +1200

    1、modify vite settings to start Network as local IP

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          chrome: "49",
-          ios: "10",
-        },
-      },
-    ],
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  plugins: [react()],
-});
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					chrome: '49',
+					ios: '10',
+				},
+			},
+		],
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	plugins: [react()],
+	server: {
+		host: '0.0.0.0',
+	},
+})
