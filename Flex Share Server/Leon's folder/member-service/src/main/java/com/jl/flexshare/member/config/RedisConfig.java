31a0bc32cf73090982d2e5b20d88efa500af7693
JL37776 <qik236899@gmail.com>
2025-09-02 09:25:52 +1200

    feat: add MailService.java @ RedisConfig.java

---
+++ Flex Share Server/Leon's folder/member-service/src/main/java/com/jl/flexshare/member/config/RedisConfig.java
@@ -0,0 +1,5 @@
+package com.jl.flexshare.member.config;
+
+
+public class RedisConfig {
+}
---
+++ Flex Share Server/Leon's folder/member-service/src/main/java/com/jl/flexshare/member/service/MailService.java
@@ -0,0 +1,27 @@
+package com.jl.flexshare.member.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void send(String to, String subject, String content) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, false);
+        helper.setFrom("leon.lee.dev@gmail.com", "flex share server");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, false);
+        mailSender.send(message);
+    }
+}
