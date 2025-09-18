8a66d890fbdc604970b62452635f5b785259713f
ttang1024 <1304126527@qq.com>
2025-09-18 12:17:21 +1200

    fix sw

---
+++ web/public/sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'flex-share-pwa-cache-v2'
+const urlsToCache = ['/', '/index.html', '/manifest.json', 'car.svg', 'logo192.png', 'logo512.png']
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(cache => cache.addAll(urlsToCache))
+			.then(() => self.skipWaiting())
+			.catch(err => console.error('error:', err)),
+	)
+})
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(cacheNames => {
+				return Promise.all(
+					cacheNames.map(cache => {
+						if (cache !== CACHE_NAME) {
+							return caches.delete(cache)
+						}
+					}),
+				)
+			})
+			.then(() => self.clients.claim()),
+	)
+})
+
+self.addEventListener('fetch', event => {
+	if (event.request.mode === 'navigate') {
+		event.respondWith(
+			fetch(event.request).catch(() => {
+				return caches.match('/index.html')
+			}),
+		)
+		return
+	}
+
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			const fetchPromise = fetch(event.request).then(networkResponse => {
+				caches.open(CACHE_NAME).then(cache => {
+					cache.put(event.request, networkResponse.clone())
+					return networkResponse
+				})
+			})
+
+			return response || fetchPromise
+		}),
+	)
+})
