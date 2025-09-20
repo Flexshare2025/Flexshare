f8d253fe3d07de6bbf523c1bb911dbda4df00099
ttang1024 <1304126527@qq.com>
2025-09-21 10:50:20 +1200

    remove sw

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,19 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('./sw.js')
-//       .then((registration) => {
-//         console.log('ServiceWorker success: ', registration.scope);
-//       })
-//       .catch((err) => {
-//         console.log('ServiceWorker error: ', err);
-//       });
-//   });
-// }
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
