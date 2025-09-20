82c629060a2ddd265a6a3d49307c5579c5d988e1
ttang1024 <1304126527@qq.com>
2025-09-21 10:44:26 +1200

    remove serviceWorker

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then((registration) => {
-        console.log('ServiceWorker success: ', registration.scope);
-      })
-      .catch((err) => {
-        console.log('ServiceWorker error: ', err);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('./sw.js')
+//       .then((registration) => {
+//         console.log('ServiceWorker success: ', registration.scope);
+//       })
+//       .catch((err) => {
+//         console.log('ServiceWorker error: ', err);
+//       });
+//   });
+// }
 
 
 createRoot(document.getElementById('root')).render(
